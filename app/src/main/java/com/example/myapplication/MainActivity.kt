32dd0652e3ef8fe6fc49b558e427c012aa34634d
lee yan jang <yanjanglee@gmail.com>
2019-12-09 13:08:01 +0800

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//
 
         premium = ViewModelProviders.of(this).get(PremiumVeiwModel::class.java)
 
